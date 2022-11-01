7599ad5bf7eaf9d73feeee8139a6b05b41a4e2a2
diego <diegofelipesales23@gmail.com>
2022-11-01 14:18:00 -0300

    inserindo html no node

--- coreModules/retornando_html/index.js
+++ coreModules/retornando_html/index.js
@@ -3,8 +3,9 @@ const http = require("http");
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.write("Oi http");
-  res.end();
+  res.statusCode = 200;
+  res.setHeader("content-Type", "text/html");
+  res.end("<h1>Server com html</h1>");
 });
 
 server.listen(port, () => {
